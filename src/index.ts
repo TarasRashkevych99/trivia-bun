1a813bbdef5ff01c367ed080b0e5cc055101dc88
MarcoRcc <marcoroccon01@gmail.com>
2023-10-18 12:15:18 +0200

    nickname changing and player db modified

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //@ts-nocheck
 import { Elysia } from "elysia";
+import { type } from "os";
 
 const DATABASE_PATH = "database.json";
 
@@ -37,10 +38,32 @@ app.post("/questions/:questionId/answer", async (context) => {
   // Ricordarsi anche di passare l'utende in qualche modo (dal body è il meno peggio)
   // *credo sia meglio inserire un campo "isCorrect" in tutte le risposte e poi ritornare quello*
 
-  return question.correct_answer === context.body.questionId
+  return question.correct_answer === context.body.myAnswer
 })
 
 // fare la put che cambia il nickname del player
+app.put("/player/:playerId/changeNickname", async (context) => {
+  const dbFile = Bun.file(DATABASE_PATH);
+  const database = await dbFile.json();
+  const name = context.body.nickname;
+
+  const player = database.player.find(player => {
+    return player.player_id == context.params.playerId
+  })
+
+  if (name != undefined && player != undefined){
+    player.nickname = name;
+    const updatedJson = JSON.stringify(database, null, 4);
+    await Bun.write(dbFile, updatedJson)
+    
+    return "Nickname Updated";
+  }else{
+    return "Error"
+  }
+
+})
+
+
 
 app.listen(3000)
 
