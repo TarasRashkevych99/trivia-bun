ff6086a6fbb871f5b93d14d7286192e509e59c6a
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2023-10-18 12:34:33 +0200

    update: refactor and extend apis

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,50 @@
 //@ts-nocheck
 import { Elysia } from "elysia";
 
+async function getDatabase() {
+  const dbFile = Bun.file(DATABASE_PATH);
+  return await dbFile.json();
+}
+
 const DATABASE_PATH = "database.json";
+const PLAYER_ID = 0;
 
 const app = new Elysia()
+
+const database = await getDatabase()
 
 app.get("/", () => "Hello Elysia");
 
 app.get("/questions", async (context) => {
-  const dbFile = Bun.file(DATABASE_PATH);
-  const database = await dbFile.json();
-  
   return database.questions
 })
 
 app.get("/questions/:questionId", async (context) => {
-  const dbFile = Bun.file(DATABASE_PATH);
-  const database = await dbFile.json();
-  
+  let questionId = parseInt(context.params.questionId)
   const question = database.questions.find(question => {
-    return question.id == context.params.questionId
+    return question.id === questionId
   })
 
   return question
 })
 
 app.post("/questions/:questionId/answer", async (context) => {
-  const dbFile = Bun.file(DATABASE_PATH);
-  const database = await dbFile.json();
-  
   const question = database.questions.find(question => {
     return question.id == context.params.questionId
   })
 
+  if (!question) {
+    return "Question not found"
+  }
+
+  const answer = {
+    player_id: PLAYER_ID,
+    question_id: question.id,
+    answer: context.body.answer
+  }
+
+  database.answers.push(answer)
+  
   // Manca da creare e caricare nel database l'oggetto risposta.
   // Ricordarsi anche di passare l'utende in qualche modo (dal body è il meno peggio)
   // *credo sia meglio inserire un campo "isCorrect" in tutte le risposte e poi ritornare quello*
