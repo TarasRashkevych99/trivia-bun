c296eb170520b8ecd1bab454b52b4caa18b6af34
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2023-10-22 18:38:19 +0200

    delete .env

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,15 @@
 import Bun from 'bun';
 
 const PLAYER_ID = 0;
+const DATABASE_PATH = "database.json"
 
 async function getDatabase() {
-    const dbFile = Bun.file(process.env.DATABASE_PATH!);
+    const dbFile = Bun.file(DATABASE_PATH);
     return await dbFile.json();
 }
 
 async function updateDatabase(database: any) {
-    const dbFile = Bun.file(process.env.DATABASE_PATH!);
+    const dbFile = Bun.file(DATABASE_PATH);
     const updatedJson = JSON.stringify(database, null, 4);
     await Bun.write(dbFile, updatedJson);
 }
